c127672add3f44060d20e64228938910e4850694
Theodoscus <theodosis14@yahoo.gr>
2024-03-26 18:28:20 +0200

    Working number buttons, screen display, and all calculations with integer numbers

--- script.js
+++ script.js
@@ -51,6 +51,38 @@ btnadd.addEventListener('click', function() {
     
 });
 
+btnsub.addEventListener('click', function() {
+    if (calcflag==false){
+        calcScreen.textContent += '-';
+        calculation='substract';
+        calcflag=true;
+    }
+    
+});
+
+btnmul.addEventListener('click', function() {
+    if (calcflag==false){
+        calcScreen.textContent += 'x';
+        calculation='multiply';
+        calcflag=true;
+    }
+    
+});
+
+btndiv.addEventListener('click', function() {
+    if (calcflag==false){
+        calcScreen.textContent += '/';
+        calculation='divide';
+        calcflag=true;
+    }
+    
+});
+
+btndot.addEventListener('click', function() {
+        calcScreen.textContent += '.';
+});
+
+
 btnequal.addEventListener('click', function() {
     if((calcflag==true)&&(calculation=='add')){
         const regex = /(\d+(\.\d+)?)\+(\d+(\.\d+)?)/; 
@@ -61,12 +93,30 @@ btnequal.addEventListener('click', function() {
         resultScreen.textContent=sum;
 
     }else if((calcflag==true)&&(calculation=='substract')){
-        const regex = /(\d+(\.\d+)?)\+(\d+(\.\d+)?)/; 
+        const regex = /(\d+(\.\d+)?)\-(\d+(\.\d+)?)/; 
         const match = calcScreen.textContent.match(regex);
         num1 = parseFloat(match[1]); 
         num2 = parseFloat(match[3]);
-        const sum = add(num1, num2);
+        const sum = substract(num1, num2);
         resultScreen.textContent=sum;
+    }else if((calcflag==true)&&(calculation=='multiply')){
+        const regex = /(\d+(\.\d+)?)\x(\d+(\.\d+)?)/; 
+        const match = calcScreen.textContent.match(regex);
+        num1 = parseFloat(match[1]); 
+        num2 = parseFloat(match[3]);
+        const sum = multiply(num1, num2);
+        resultScreen.textContent=sum;
+    }else if((calcflag==true)&&(calculation=='divide')){
+        const regex = /(\d+(\.\d+)?)\/(\d+(\.\d+)?)/; 
+        const match = calcScreen.textContent.match(regex);
+        num1 = parseFloat(match[1]); 
+        num2 = parseFloat(match[3]);
+        if(num2!=0){
+            const sum = divide(num1, num2);
+            resultScreen.textContent=sum;
+        }else{
+            resultScreen.textContent='MathError';
+        }
     }
 });
 
@@ -84,7 +134,69 @@ btn1.addEventListener('click', function() {
     }
 });
 
+btn2.addEventListener('click', function() {
+    if(calcScreen.textContent=='0'){
+        calcScreen.textContent='2'  
+    }else if(calcScreen.textContent.length<12){
+        calcScreen.textContent=calcScreen.textContent+2;
+    }
+});
 
+btn3.addEventListener('click', function() {
+    if(calcScreen.textContent=='0'){
+        calcScreen.textContent='3'  
+    }else if(calcScreen.textContent.length<12){
+        calcScreen.textContent=calcScreen.textContent+3;
+    }
+});
+
+btn4.addEventListener('click', function() {
+    if(calcScreen.textContent=='0'){
+        calcScreen.textContent='4'  
+    }else if(calcScreen.textContent.length<12){
+        calcScreen.textContent=calcScreen.textContent+4;
+    }
+});
+
+btn5.addEventListener('click', function() {
+    if(calcScreen.textContent=='0'){
+        calcScreen.textContent='5'  
+    }else if(calcScreen.textContent.length<12){
+        calcScreen.textContent=calcScreen.textContent+5;
+    }
+});
+
+btn6.addEventListener('click', function() {
+    if(calcScreen.textContent=='0'){
+        calcScreen.textContent='6'  
+    }else if(calcScreen.textContent.length<12){
+        calcScreen.textContent=calcScreen.textContent+6;
+    }
+});
+
+btn7.addEventListener('click', function() {
+    if(calcScreen.textContent=='0'){
+        calcScreen.textContent='7'  
+    }else if(calcScreen.textContent.length<12){
+        calcScreen.textContent=calcScreen.textContent+7;
+    }
+});
+
+btn8.addEventListener('click', function() {
+    if(calcScreen.textContent=='0'){
+        calcScreen.textContent='8'  
+    }else if(calcScreen.textContent.length<12){
+        calcScreen.textContent=calcScreen.textContent+8;
+    }
+});
+
+btn9.addEventListener('click', function() {
+    if(calcScreen.textContent=='0'){
+        calcScreen.textContent='9'  
+    }else if(calcScreen.textContent.length<12){
+        calcScreen.textContent=calcScreen.textContent+9;
+    }
+});
 
 function add(num1, num2){
     return num1+num2;
@@ -92,4 +204,12 @@ function add(num1, num2){
 
 function substract(num1, num2){
     return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
 }
